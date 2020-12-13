269052e405c3f7b43bd02f01b89e0eeb7564e60e
dovimaj <dovimajowka@gmail.com>
2020-12-13 02:19:59 +0200

    added ship tests

--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -14,3 +14,44 @@ it("Ship hasSunk() and hit() works", () => {
   a.hit([1, 0]);
   expect(a.hasSunk()).toBeTruthy();
 });
+
+it("get vertical returns true or false", () => {
+  const a = Ship(
+    "w",
+    1,
+    [
+      [0, 0],
+      [1, 0],
+    ],
+    true
+  );
+  expect(a.getVertical()).toBe(true);
+});
+
+it("get length to be 2", () => {
+  const a = Ship(
+    "w",
+    2,
+    [
+      [0, 0],
+      [1, 0],
+    ],
+    true
+  );
+  expect(a.getLength()).toBe(2);
+});
+
+it("getAllPositions to be array of arrays ", () => {
+  const a = Ship(
+    "w",
+    1,
+    [
+      [0, 0],
+      [1, 0],
+    ],
+    true
+  );
+  const isArrayOfArrays =
+    Array.isArray(a.getAllPosition()) && Array.isArray(a.getAllPosition()[0]);
+  expect(isArrayOfArrays).toBe(true);
+});
