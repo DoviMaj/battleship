24becdd02bb2aa64d060665319e6a8382784ffef
dovimaj <dovimajowka@gmail.com>
2020-12-13 02:20:31 +0200

    added gameboard tests

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,56 +1,79 @@
 import { Gameboard } from "../modules/Gameboard.js";
 
-it("Gameboard only adds ship if position is available ", () => {
+it("Gameboard adds ships to gameboard", () => {
   const a = Gameboard();
-  a.addShip("w", 2, [0, 0], true);
-  a.addShip("b", 2, [1, 0], true);
+  a.addShip(
+    "si",
+    2,
+    [
+      [1, 2],
+      [1, 3],
+    ],
+    true
+  );
   const g = a.getGameboard();
-  // expect(g).toBe();
-  expect(g[3][0]).toBe("");
+  expect(g[1][2] && g[1][3]).toBe("ship");
 });
 
-it("Gameboard adds ships verticaly", () => {
+it("Gameboard attack succesfully", () => {
   const a = Gameboard();
-  a.addShip("w", 3, [0, 0], true);
-  a.addShip("b", 3, [0, 1], true);
-  const g = a.getGameboard();
-  // expect(g).toBe();
-  expect(g[0][1] && g[1][1] && g[2][1]).toBe("ship");
-});
-
-it("Gameboard adds ships horizontaly", () => {
-  const a = Gameboard();
-  a.addShip("w", 2, [0, 0], false);
-  const g = a.getGameboard();
-  // expect(g).toBe();
-  expect(g[0][0] && g[0][1]).toBe("ship");
-});
-
-it("Gameboard sucessful attack", () => {
-  const a = Gameboard();
-  a.addShip("w", 2, [0, 0], false);
+  a.addShip(
+    "w",
+    2,
+    [
+      [0, 0],
+      [0, 1],
+    ],
+    false
+  );
   a.receiveAttack(0, 0);
-  const g = a.getGameboard();
-  // expect(g).toBe();
-  expect(g[0][0]).toBe("attacked");
+  expect(a.getGameboard()[0][0]).toBe("attacked");
 });
 
 it("Gameboard missed attack", () => {
   const a = Gameboard();
-  a.addShip("w", 2, [0, 0], false);
+  a.addShip(
+    "w",
+    2,
+    [
+      [0, 0],
+      [0, 1],
+    ],
+    false
+  );
   a.receiveAttack(1, 0);
-  const g = a.getGameboard();
-  // expect(g).toBe();
-  expect(g[1][0]).toBe("missed");
+  expect(a.getGameboard()[1][0]).toBe("missed");
 });
 
 it("Gameboard reports if all of their ships have sunk", () => {
   const a = Gameboard();
-  a.addShip("w", 2, [0, 0], true);
-  a.addShip("w", 2, [0, 1], true);
+  a.addShip(
+    "w",
+    2,
+    [
+      [0, 0],
+      [0, 1],
+    ],
+    false
+  );
+  a.addShip(
+    "w",
+    2,
+    [
+      [1, 2],
+      [1, 3],
+    ],
+    false
+  );
+  a.receiveAttack(1, 2);
+  a.receiveAttack(1, 3);
   a.receiveAttack(0, 0);
-  a.receiveAttack(1, 0);
   a.receiveAttack(0, 1);
-  a.receiveAttack(1, 1);
   expect(a.haveAllSunk()).toBeTruthy();
+});
+
+it("Gameboard addShipsRandomly() defines ship position as array of arrays", () => {
+  const a = Gameboard();
+  a.addShipsRandomly();
+  expect(Array.isArray(a.getShips()[0].p)).toBe(true);
 });
