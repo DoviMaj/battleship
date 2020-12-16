09aef4cccba3b2791e43f958002bec430a632f65
dovimaj <dovimajowka@gmail.com>
2020-12-16 20:09:21 +0200

    player tests

--- src/tests/Player.test.js
+++ src/tests/Player.test.js
@@ -1,5 +1,16 @@
+import { Gameboard } from "../modules/Gameboard.js";
 import { Player } from "../modules/Player.js";
 
-it("Player", () => {
-  expect(Player()).toBe();
+describe("Player module", () => {
+  it("Human Player attacks oponent gameboard", () => {
+    const p = Player("Human");
+    const c = Gameboard();
+    p.attack(1, 2, c);
+    expect(c.getGameboard()[1][2]).toBe("missed");
+  });
+  it("PC Player attacks oponent gameboard", () => {
+    const p = Player("PC");
+    const c = Gameboard();
+    expect(Array.isArray(p.attack(1, 2, c))).toBeTruthy;
+  });
 });
