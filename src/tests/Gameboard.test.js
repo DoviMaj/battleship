51ee3cfcc405513b9c94e00f4e02fac17dd23e81
dovimaj <dovimajowka@gmail.com>
2020-12-16 20:09:44 +0200

    added scope to tests

--- src/tests/Gameboard.test.js
+++ src/tests/Gameboard.test.js
@@ -1,79 +1,81 @@
 import { Gameboard } from "../modules/Gameboard.js";
 
-it("Gameboard adds ships to gameboard", () => {
-  const a = Gameboard();
-  a.addShip(
-    "si",
-    2,
-    [
-      [1, 2],
-      [1, 3],
-    ],
-    true
-  );
-  const g = a.getGameboard();
-  expect(g[1][2] && g[1][3]).toBe("ship");
-});
+describe("Gameboard factory", () => {
+  it("Gameboard adds ships to gameboard", () => {
+    const a = Gameboard();
+    a.addShip(
+      "si",
+      2,
+      [
+        [1, 2],
+        [1, 3],
+      ],
+      true
+    );
+    const g = a.getGameboard();
+    expect(g[1][2] && g[1][3]).toBe("ship");
+  });
 
-it("Gameboard attack succesfully", () => {
-  const a = Gameboard();
-  a.addShip(
-    "w",
-    2,
-    [
-      [0, 0],
-      [0, 1],
-    ],
-    false
-  );
-  a.receiveAttack(0, 0);
-  expect(a.getGameboard()[0][0]).toBe("attacked");
-});
+  it("Gameboard attack succesfully", () => {
+    const a = Gameboard();
+    a.addShip(
+      "w",
+      2,
+      [
+        [0, 0],
+        [0, 1],
+      ],
+      false
+    );
+    a.receiveAttack(0, 0);
+    expect(a.getGameboard()[0][0]).toBe("attacked");
+  });
 
-it("Gameboard missed attack", () => {
-  const a = Gameboard();
-  a.addShip(
-    "w",
-    2,
-    [
-      [0, 0],
-      [0, 1],
-    ],
-    false
-  );
-  a.receiveAttack(1, 0);
-  expect(a.getGameboard()[1][0]).toBe("missed");
-});
+  it("Gameboard missed attack", () => {
+    const a = Gameboard();
+    a.addShip(
+      "w",
+      2,
+      [
+        [0, 0],
+        [0, 1],
+      ],
+      false
+    );
+    a.receiveAttack(1, 0);
+    expect(a.getGameboard()[1][0]).toBe("missed");
+  });
 
-it("Gameboard reports if all of their ships have sunk", () => {
-  const a = Gameboard();
-  a.addShip(
-    "w",
-    2,
-    [
-      [0, 0],
-      [0, 1],
-    ],
-    false
-  );
-  a.addShip(
-    "w",
-    2,
-    [
-      [1, 2],
-      [1, 3],
-    ],
-    false
-  );
-  a.receiveAttack(1, 2);
-  a.receiveAttack(1, 3);
-  a.receiveAttack(0, 0);
-  a.receiveAttack(0, 1);
-  expect(a.haveAllSunk()).toBeTruthy();
-});
+  it("Gameboard reports if all of their ships have sunk", () => {
+    const a = Gameboard();
+    a.addShip(
+      "w",
+      2,
+      [
+        [0, 0],
+        [0, 1],
+      ],
+      false
+    );
+    a.addShip(
+      "w",
+      2,
+      [
+        [1, 2],
+        [1, 3],
+      ],
+      false
+    );
+    a.receiveAttack(1, 2);
+    a.receiveAttack(1, 3);
+    a.receiveAttack(0, 0);
+    a.receiveAttack(0, 1);
+    expect(a.haveAllSunk()).toBeTruthy();
+  });
 
-it("Gameboard addShipsRandomly() defines ship position as array of arrays", () => {
-  const a = Gameboard();
-  a.addShipsRandomly();
-  expect(Array.isArray(a.getShips()[0].p)).toBe(true);
+  it("Gameboard addShipsRandomly() defines ship position as array of arrays", () => {
+    const a = Gameboard();
+    a.addShipsRandomly();
+    expect(Array.isArray(a.getShips()[0].p)).toBeTruthy();
+  });
 });
--- src/tests/Ship.test.js
+++ src/tests/Ship.test.js
@@ -1,57 +1,59 @@
 import { Ship } from "../modules/Ship.js";
 
-it("Ship hasSunk() and hit() works", () => {
-  const a = Ship(
-    "w",
-    1,
-    [
-      [0, 0],
-      [1, 0],
-    ],
-    true
-  );
-  a.hit([0, 0]);
-  a.hit([1, 0]);
-  expect(a.hasSunk()).toBeTruthy();
-});
+describe("Ship factory", () => {
+  it("Ship hasSunk() and hit() works", () => {
+    const a = Ship(
+      "w",
+      1,
+      [
+        [0, 0],
+        [1, 0],
+      ],
+      true
+    );
+    a.hit([0, 0]);
+    a.hit([1, 0]);
+    expect(a.hasSunk()).toBeTruthy();
+  });
 
-it("get vertical returns true or false", () => {
-  const a = Ship(
-    "w",
-    1,
-    [
-      [0, 0],
-      [1, 0],
-    ],
-    true
-  );
-  expect(a.getVertical()).toBe(true);
-});
+  it("get vertical returns true or false", () => {
+    const a = Ship(
+      "w",
+      1,
+      [
+        [0, 0],
+        [1, 0],
+      ],
+      true
+    );
+    expect(a.getVertical()).toBe(true);
+  });
 
-it("get length to be 2", () => {
-  const a = Ship(
-    "w",
-    2,
-    [
-      [0, 0],
-      [1, 0],
-    ],
-    true
-  );
-  expect(a.getLength()).toBe(2);
-});
+  it("get length to be 2", () => {
+    const a = Ship(
+      "w",
+      2,
+      [
+        [0, 0],
+        [1, 0],
+      ],
+      true
+    );
+    expect(a.getLength()).toBe(2);
+  });
 
-it("getAllPositions to be array of arrays ", () => {
-  const a = Ship(
-    "w",
-    1,
-    [
-      [0, 0],
-      [1, 0],
-    ],
-    true
-  );
-  const isArrayOfArrays =
-    Array.isArray(a.getAllPosition()) && Array.isArray(a.getAllPosition()[0]);
-  expect(isArrayOfArrays).toBe(true);
+  it("getAllPositions to be array of arrays ", () => {
+    const a = Ship(
+      "w",
+      1,
+      [
+        [0, 0],
+        [1, 0],
+      ],
+      true
+    );
+    const isArrayOfArrays =
+      Array.isArray(a.getAllPosition()) && Array.isArray(a.getAllPosition()[0]);
+    expect(isArrayOfArrays).toBe(true);
+  });
 });
